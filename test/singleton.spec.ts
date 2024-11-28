601e29fa2bfc9357017ad86bf7f16d6ba288e152
rafaelpaesss <94455282+rafaelpaesss@users.noreply.github.com>
2024-11-27 23:29:03 -0300

    Update singleton.spec.ts

--- test/singleton.spec.ts
+++ test/singleton.spec.ts
@@ -1,17 +1,19 @@
-import { PrismaClient } from '@prisma/client';
-import { DeepMockProxy, mockDeep, mockReset } from 'jest-mock-extended';
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb"; // Importa o cliente DynamoDB
+import { DeepMockProxy, mockDeep, mockReset } from "jest-mock-extended";
 
-// Mock do Prisma
-jest.mock('./client', () => ({
-  __esModule: true,
-  default: mockDeep<PrismaClient>(), // Criação do mock do PrismaClient
+// Mock do DynamoDB
+jest.mock("@aws-sdk/client-dynamodb", () => ({
+  DynamoDBClient: jest.fn().mockImplementation(() => ({
+    // Aqui você pode adicionar os métodos mockados do DynamoDB que você está usando, por exemplo:
+    send: jest.fn().mockResolvedValue({ /* resposta mockada */ }),
+  })),
 }));
 
-// Recupera a instância do prisma mockado
-export const prismaMock = prisma as unknown as DeepMockProxy<PrismaClient>;
+// Recupera a instância do DynamoDB mockado
+export const dynamoDBMock = DynamoDBClient as unknown as DeepMockProxy<DynamoDBClient>;
 
 beforeEach(() => {
-  mockReset(prismaMock); // Reseta o prismaMock antes de cada teste
+  mockReset(dynamoDBMock); // Reseta o mock antes de cada teste
 });
 
 describe('Singleton', () => {
