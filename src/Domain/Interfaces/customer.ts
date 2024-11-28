aefe17e74e91e939001adc5144a28aeb59d209fa
rafaelpaesss <94455282+rafaelpaesss@users.noreply.github.com>
2024-11-27 23:57:05 -0300

    Update customer.ts

--- src/Domain/Interfaces/customer.ts
+++ src/Domain/Interfaces/customer.ts
@@ -1,11 +1,12 @@
-// src/Domain/Interfaces/customer.ts
-export interface Customers {
-  id: number;
-  name: string | null;
-  email: string | null;
-  cpf: string | null;
-  isAdmin: boolean;
-  createdAt: Date;
-  updatedAt: Date;
-  password?: string | null;
+// src/Domain/entities/customer.entity.ts
+
+export interface Customer {
+  cpf: string;  // CPF como chave única
+  name: string;  // Nome do cliente
+  email: string;  // Email do cliente
+  password: string;  // Senha do cliente
+  phone?: string;  // Telefone opcional
+  address?: string;  // Endereço opcional
+  createdAt: string;  // Data de criação do cadastro
+  updatedAt: string;  // Data da última atualização
 }
