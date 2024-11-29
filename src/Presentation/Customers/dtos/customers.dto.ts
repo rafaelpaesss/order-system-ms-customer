d00337289d4832730043126beaab0981c7f0df26
rafaelpaesss <94455282+rafaelpaesss@users.noreply.github.com>
2024-11-28 23:23:26 -0300

    Update customers.dto.ts

--- src/Presentation/Customers/dtos/customers.dto.ts
+++ src/Presentation/Customers/dtos/customers.dto.ts
@@ -1,45 +1,7 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsBoolean, IsString, IsNotEmpty } from 'class-validator';
+import { IsString, IsNotEmpty } from 'class-validator';
 
-// src/Presentation/Customers/dto/customers.dto.ts
-export class CustomersDto {
-  @ApiProperty({
-    description: 'Nome completo do cliente',
-    type: String,
-  })
+export class GetCustomerDto {
   @IsString()
   @IsNotEmpty()
-  name: string;
-
-  @ApiProperty({
-    description: 'Endereço de email do cliente',
-    type: String,
-  })
-  @IsString()
-  @IsNotEmpty()
-  email: string;
-
-  @ApiProperty({
-    description: 'CPF do cliente sem máscara',
-    type: String,
-  })
-  @IsString()
-  @IsNotEmpty()
-  cpf: string;
-
-  @ApiProperty({
-    description: 'Indica se o cliente é um administrador',
-    type: Boolean,
-  })
-  @IsBoolean()
-  @IsNotEmpty()
-  isAdmin: boolean;
-
-  @ApiProperty({
-    description: 'Senha do cliente',
-    type: String,
-  })
-  @IsString()
-  @IsNotEmpty()
-  password: string;
+  password: string; // Validação para o password
 }
