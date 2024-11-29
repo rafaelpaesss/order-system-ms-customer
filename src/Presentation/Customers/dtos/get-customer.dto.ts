0c74b81141e2086f4e988e679b605d4d46db8fcb
rafaelpaesss <94455282+rafaelpaesss@users.noreply.github.com>
2024-11-28 23:39:59 -0300

    Update get-customer.dto.ts

--- src/Presentation/Customers/dtos/get-customer.dto.ts
+++ src/Presentation/Customers/dtos/get-customer.dto.ts
@@ -1,6 +1,6 @@
 import { IsString, IsNotEmpty } from 'class-validator';
 
-export class CustomerDto {
+export class CustomersDto {
   @IsString()
   @IsNotEmpty()
   password: string; // Validação para o password
