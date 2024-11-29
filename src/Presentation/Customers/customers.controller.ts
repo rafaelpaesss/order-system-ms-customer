5c14e715a21034b01ec1002c33a1e2ebd981dc81
rafaelpaesss <94455282+rafaelpaesss@users.noreply.github.com>
2024-11-28 23:37:33 -0300

    Update customers.controller.ts

--- src/Presentation/Customers/customers.controller.ts
+++ src/Presentation/Customers/customers.controller.ts
@@ -9,7 +9,7 @@ import {
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { CustomerService } from '../../Application/services/customer.service';
-import { GetCustomerDto } from './dtos/get-customer.dto';
+import { CustomersDto } from './dtos/get-customer.dto';
 
 @ApiTags('Clientes')
 @Controller('customers')
