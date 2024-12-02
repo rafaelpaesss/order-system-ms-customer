5a66d5e3bc508d7eeeabc436c7327be7cd88db95
rafaelpaesss <94455282+rafaelpaesss@users.noreply.github.com>
2024-12-02 15:21:48 -0300

    Update customers.dto.ts

--- src/Presentation/Customers/dtos/customers.dto.ts
+++ src/Presentation/Customers/dtos/customers.dto.ts
@@ -2,4 +2,10 @@ export class CustomerDto {
   cpf: string;
   name: string;
   email: string;
+  
+  constructor(cpf: string, name: string, email: string) {
+    this.cpf = cpf;
+    this.name = name;
+    this.email = email;
+  }
 }
