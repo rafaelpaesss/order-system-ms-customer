4bd4f3bdcbbd152cd383a04eb735e5f91caaacf0
rafaelpaesss <94455282+rafaelpaesss@users.noreply.github.com>
2024-12-02 11:59:18 -0300

    Update swagger.ts

--- src/swagger.ts
+++ src/swagger.ts
@@ -1,29 +1,38 @@
-import { INestApplication } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import express from 'express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import { CustomersController } from './Presentation/Customers/customers.controller';
 
-export default async function (app: INestApplication) {
-  const configService = app.get(ConfigService);
+const app = express();
+app.use(express.json());
 
-  const env = configService.get<string>('ENVIRONMENT');
-
-  if (env !== 'prd') {
-    const documentBuild = new DocumentBuilder()
-      .setTitle('API Backend')
-      .setDescription('API desenvolvida para o projeto pos-tech.')
-      .setVersion('1.0')
-      .build();
-
-    const document = SwaggerModule.createDocument(app, documentBuild);
-
-    SwaggerModule.setup('api/swagger', app, document, {
-      customSiteTitle: 'API Backend',
-      swaggerOptions: {
-        syntaxHighlight: {
-          activate: true,
-          theme: 'tomorrow-night',
-        },
+// Swagger configuration
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Customer API',
+      version: '1.0.0',
+      description: 'API for managing customers in DynamoDB',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000', // Ou a URL do seu servidor EKS
       },
-    });
-  }
-}
+    ],
+  },
+  apis: ['./src/Presentation/Customers/customers.controller.ts'], // Path para o arquivo que contém as anotações da API
+};
+
+const specs = swaggerJsdoc(options);
+
+// Swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+
+// Definindo os endpoints
+app.post('/customers', CustomersController.createCustomer);
+app.get('/customers/:cpf', CustomersController.getCustomer);
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
