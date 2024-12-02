da5f9c949ff377fd9b23c34ac351134b808c6e8f
rafaelpaesss <94455282+rafaelpaesss@users.noreply.github.com>
2024-12-02 12:08:54 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,44 +1,11 @@
-import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { APP_GUARD } from '@nestjs/core';
-import { EventEmitterModule } from '@nestjs/event-emitter';
-import { PrismaService } from './Infrastructure/Apis/prisma.service';
-import { RolesGuard } from './Infrastructure/Guard/roles.guard';
-import { CustomersModule } from './Presentation/Customers/customers.module';
-import { HealthModule } from './Presentation/Health/health.module';
+import { CustomersController } from './Presentation/Customers/customers.controller';
+import { CustomerService } from './Application/services/customer.service';
+import { DynamoDBService } from './Infrastructure/Apis/dynamodb.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    EventEmitterModule.forRoot({
-      // set this to `true` to use wildcards
-      wildcard: false,
-      // the delimiter used to segment namespaces
-      delimiter: '.',
-      // set this to `true` if you want to emit the newListener event
-      newListener: false,
-      // set this to `true` if you want to emit the removeListener event
-      removeListener: false,
-      // the maximum amount of listeners that can be assigned to an event
-      maxListeners: 10,
-      // show event name in memory leak message when more than maximum amount of listeners is assigned
-      verboseMemoryLeak: true,
-      // disable throwing uncaughtException if an error event is emitted and it has no listeners
-      ignoreErrors: false,
-    }),
-    HttpModule,
-    HealthModule,
-    CustomersModule
-  ],
-  providers: [
-    PrismaService,
-    {
-      provide: APP_GUARD,
-      useClass: RolesGuard,
-    },
-  ],
-  exports: [],
-  controllers: [],
+  imports: [],
+  controllers: [CustomersController],
+  providers: [CustomerService, DynamoDBService],
 })
 export class AppModule {}
