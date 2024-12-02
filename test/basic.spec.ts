f9dc6a9b38fc38a858ab260d24219b4cd1b73680
rafaelpaesss <94455282+rafaelpaesss@users.noreply.github.com>
2024-12-02 12:10:38 -0300

    Update basic.spec.ts

--- test/basic.spec.ts
+++ test/basic.spec.ts
@@ -1,5 +1,49 @@
-describe('Basic Test', () => {
-  it('should pass', () => {
-    expect(true).toBe(true);
+import { Test, TestingModule } from '@nestjs/testing';
+import { CustomersController } from '../src/Presentation/Customers/customers.controller';
+import { CustomerService } from '../src/Application/services/customer.service';
+import { DynamoDBService } from '../src/Infrastructure/Apis/dynamodb.service';
+import { CreateCustomerDto } from '../src/Presentation/Customers/dtos/create-customer.dto';
+
+describe('CustomersController', () => {
+  let controller: CustomersController;
+  let service: CustomerService;
+
+  beforeEach(async () => {
+    // Configura a instância do NestJS para o controlador e o serviço
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [CustomersController],
+      providers: [CustomerService, DynamoDBService],
+    }).compile();
+
+    controller = module.get<CustomersController>(CustomersController);
+    service = module.get<CustomerService>(CustomerService);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  describe('createCustomer', () => {
+    it('should create a customer', async () => {
+      const createCustomerDto: CreateCustomerDto = {
+        cpf: '12345678900',
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: 'password123',
+      };
+
+      // Mock do serviço para retornar o cliente criado
+      jest.spyOn(service, 'createCustomer').mockResolvedValue(createCustomerDto);
+
+      const response = await controller.createCustomer({ body: createCustomerDto } as any, {} as any);
+
+      // Verifica se a resposta do controlador é a esperada
+      expect(response.status).toBe(201);
+      expect(response.body).toEqual({
+        cpf: '12345678900',
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+      });
+    });
   });
 });
