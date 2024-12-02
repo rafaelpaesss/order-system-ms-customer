bf0a842b7d49793310ccba48446df8374e053311
rafaelpaesss <94455282+rafaelpaesss@users.noreply.github.com>
2024-12-02 12:05:16 -0300

    Update create-customer.dto.ts

--- src/Presentation/Customers/dtos/create-customer.dto.ts
+++ src/Presentation/Customers/dtos/create-customer.dto.ts
@@ -1,20 +1,6 @@
-// src/Application/dtos/create-customer.dto.ts
-import { IsNotEmpty, IsString, IsEmail, IsOptional } from 'class-validator';
-
 export class CreateCustomerDto {
-  @IsNotEmpty()
-  @IsString()
-  cpf: string; // CPF do cliente
-
-  @IsNotEmpty()
-  @IsString()
-  name: string; // Nome do cliente
-
-  @IsNotEmpty()
-  @IsString()
-  password: string; // Senha do cliente
-
-  @IsOptional()
-  @IsEmail()
-  email?: string; // Email do cliente (opcional)
+  cpf: string;
+  name: string;
+  email: string;
+  password: string;  // Senha em texto simples (não segura para produção)
 }
