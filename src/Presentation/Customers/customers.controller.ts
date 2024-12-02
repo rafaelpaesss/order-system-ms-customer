ec252c04f21f8aa62f4f0fa63a014558b3b230a7
rafaelpaesss <94455282+rafaelpaesss@users.noreply.github.com>
2024-12-02 16:05:16 -0300

    Update customers.controller.ts

--- src/Presentation/Customers/customers.controller.ts
+++ src/Presentation/Customers/customers.controller.ts
@@ -1,29 +1,41 @@
-import { Controller, Post, Get, Body, Param } from '@nestjs/common'; // Certifique-se de que o NestJS está configurado corretamente
+import { 
+  Controller, 
+  Post, 
+  Get, 
+  Body, 
+  Param, 
+  NotFoundException, 
+  BadRequestException 
+} from '@nestjs/common'; // Certifique-se de que o NestJS está configurado corretamente
 import { CreateCustomerDto } from './dtos/create-customer.dto';
 import { CustomersRepository } from '../../Domain/Repositories/customersRepository';
 import { Customer } from '../../Domain/Interfaces/customer';
 
 @Controller('customers')
 export class CustomersController {
-  private customersRepository = new CustomersRepository();
+  constructor(private readonly customersRepository: CustomersRepository) {}
 
   @Post()
   async createCustomer(@Body() createCustomerDto: CreateCustomerDto) {
-    const { cpf, name, email, password } = createCustomerDto;
-    const customer: Customer = await this.customersRepository.createCustomer(
-      cpf,
-      name,
-      email,
-      password,
-    );
-    return customer;
+    try {
+      const { cpf, name, email, password } = createCustomerDto;
+      const customer: Customer = await this.customersRepository.createCustomer(
+        cpf,
+        name,
+        email,
+        password,
+      );
+      return customer;
+    } catch (error) {
+      throw new BadRequestException('Failed to create customer');
+    }
   }
 
   @Get(':cpf')
   async getCustomer(@Param('cpf') cpf: string) {
     const customer = await this.customersRepository.getCustomerByCpf(cpf);
     if (!customer) {
-      throw new Error('Customer not found');
+      throw new NotFoundException('Customer not found');
     }
     return customer;
   }
