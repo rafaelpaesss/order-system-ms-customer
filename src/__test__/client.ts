85c88237e483228a64b48918a5bfcc2fadc9a3a5
rafaelpaesss <94455282+rafaelpaesss@users.noreply.github.com>
2024-12-03 01:39:56 -0300

    Update client.ts

--- src/__test__/client.ts
+++ src/__test__/client.ts
@@ -1,6 +1,12 @@
-import { DynamoDB } from 'aws-sdk';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 
-// Cria a instância do DynamoDB DocumentClient
-const dynamoDB = new DynamoDB.DocumentClient();
+// Configurações padrão para o cliente
+const dynamoDbClient = new DynamoDBClient({
+  region: process.env.AWS_REGION || 'us-east-1',
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
+  },
+});
 
-export default dynamoDB;
+export default dynamoDbClient;
