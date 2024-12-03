8d94dc514fc653ab51f5532e338d901fa8d3bfde
rafaelpaesss <94455282+rafaelpaesss@users.noreply.github.com>
2024-12-02 21:00:14 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // Aplicando o Global Validation Pipe (caso queira validar as entradas, mas vamos deixar sem isso por enquanto)
-  // app.useGlobalPipes(new ValidationPipe());
-
-  // Configuração de Cors para permitir requisições de outros domínios
-  app.enableCors();
-
-  // Configurar Swagger (caso queira gerar documentação da API automaticamente)
-  // const document = SwaggerModule.createDocument(app, options);
-  // SwaggerModule.setup('api', app, document);
-
-  await app.listen(3000);  // O servidor vai rodar na porta 3000
+  await app.listen(3000);
 }
-bootstrap();
+
+// Exporte o app apenas para testes
+export const app = bootstrap();
