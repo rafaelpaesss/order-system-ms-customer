892d1bea6da5aadecc844f4352ae1b8df6ff7ca1
rafaelpaesss <94455282+rafaelpaesss@users.noreply.github.com>
2024-12-03 01:18:32 -0300

    Update customers.controller.ts

--- src/Presentation/Customers/customers.controller.ts
+++ src/Presentation/Customers/customers.controller.ts
@@ -5,7 +5,7 @@ import {
   Body, 
   Param, 
   NotFoundException, 
-  BadRequestException 
+  ConflictException 
 } from '@nestjs/common'; // Certifique-se de que o NestJS está configurado corretamente
 import { CreateCustomerDto } from './dtos/create-customer.dto';
 import { CustomersRepository } from '../../Domain/Repositories/customersRepository';
@@ -15,9 +15,23 @@ import { Customer } from '../../Domain/Interfaces/customer';
 export class CustomersController {
   constructor(private readonly customersRepository: CustomersRepository) {}
 
+  /**
+   * Cria um novo cliente.
+   * 
+   * @param createCustomerDto Dados do cliente a ser criado.
+   * @returns O cliente criado.
+   */
   @Post()
-  async createCustomer(@Body() createCustomerDto: CreateCustomerDto) {
+  async createCustomer(@Body() createCustomerDto: CreateCustomerDto): Promise<Customer> {
     const { cpf, name, email, password } = createCustomerDto;
+
+    // Verifica se o cliente já existe com o CPF fornecido
+    const existingCustomer = await this.customersRepository.getCustomerByCpf(cpf);
+    if (existingCustomer) {
+      throw new ConflictException('Customer with this CPF already exists');
+    }
+
+    // Cria o cliente
     const customer: Customer = await this.customersRepository.createCustomer(
       cpf,
       name,
@@ -27,12 +41,20 @@ export class CustomersController {
     return customer;
   }
 
+  /**
+   * Recupera um cliente com base no CPF.
+   * 
+   * @param cpf CPF do cliente a ser recuperado.
+   * @returns O cliente encontrado ou lança uma exceção se não encontrado.
+   */
   @Get(':cpf')
-  async getCustomer(@Param('cpf') cpf: string) {
+  async getCustomer(@Param('cpf') cpf: string): Promise<Customer> {
     const customer = await this.customersRepository.getCustomerByCpf(cpf);
+    
     if (!customer) {
       throw new NotFoundException('Customer not found');
     }
+    
     return customer;
   }
 }
