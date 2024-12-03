b5434b05c0b2d317904cb874aae8b8faa49ae289
rafaelpaesss <94455282+rafaelpaesss@users.noreply.github.com>
2024-12-02 21:08:47 -0300

    Update basic.spec.ts

--- test/basic.spec.ts
+++ test/basic.spec.ts
@@ -1,39 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CustomersController } from '../src/Presentation/Customers/customers.controller';
 import { CustomerService } from '../src/Application/services/customer.service';
-import { DynamoDBService } from '../src/Infrastructure/Apis/dynamodb.service';
-import { CustomersRepository } from '../src/Domain/Repositories/customersRepository';
 import { CreateCustomerDto } from '../src/Presentation/Customers/dtos/create-customer.dto';
+
+// Mock do CustomersRepository
+class MockCustomerService {
+  createCustomer(createCustomerDto: CreateCustomerDto) {
+    return { ...createCustomerDto }; // Simula a criação do cliente
+  }
+}
 
 describe('CustomersController', () => {
   let controller: CustomersController;
   let service: CustomerService;
 
-  const mockCustomersRepository = {
-    createCustomer: jest.fn(),
-    getCustomerByCpf: jest.fn(),
-    updateCustomer: jest.fn(),
-  };
-
-  const mockDynamoDBService = {
-    putItem: jest.fn(),
-    getItem: jest.fn(),
-    updateItem: jest.fn(),
-  };
-
   beforeEach(async () => {
-    // Configura a instância do NestJS para o controlador e o serviço
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CustomersController],
       providers: [
-        CustomerService,
         {
-          provide: DynamoDBService,
-          useValue: mockDynamoDBService, // Mock do DynamoDBService
-        },
-        {
-          provide: CustomersRepository,
-          useValue: mockCustomersRepository, // Mock do CustomersRepository
+          provide: CustomerService,
+          useClass: MockCustomerService, // Substitui o serviço real pelo mock
         },
       ],
     }).compile();
@@ -55,18 +42,13 @@ describe('CustomersController', () => {
         password: 'password123',
       };
 
-      // Mock do serviço para retornar o cliente criado
-      jest.spyOn(service, 'createCustomer').mockResolvedValue(createCustomerDto);
+      const mockCustomerResponse = { ...createCustomerDto }; // Mocka a resposta esperada
 
+      // Chama o método do controlador
       const response = await controller.createCustomer(createCustomerDto);
 
       // Verifica se a resposta do controlador é a esperada
-      expect(response).toEqual({
-        cpf: '12345678900',
-        name: 'John Doe',
-        email: 'johndoe@example.com',
-        password: 'password123',
-      });
+      expect(response).toEqual(mockCustomerResponse);
 
       // Verifica se o serviço foi chamado com os parâmetros corretos
       expect(service.createCustomer).toHaveBeenCalledWith(createCustomerDto);
