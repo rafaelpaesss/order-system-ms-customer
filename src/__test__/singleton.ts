f9fdfded574828cb416fcb8cd1f9f8bcc33ae1f1
rafaelpaesss <94455282+rafaelpaesss@users.noreply.github.com>
2024-12-03 01:40:37 -0300

    Update singleton.ts

--- src/__test__/singleton.ts
+++ src/__test__/singleton.ts
@@ -1,17 +1,25 @@
-import { DynamoDB } from 'aws-sdk';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 
-class DynamoSingleton {
-  private static instance: DynamoDB.DocumentClient;
+class DynamoDBClientSingleton {
+  private static instance: DynamoDBClient | null = null;
 
-  // Garante que a instância seja única
-  private constructor() {}
+  private constructor() {
+    // Construtor privado para impedir instâncias externas
+  }
 
-  public static getInstance(): DynamoDB.DocumentClient {
-    if (!DynamoSingleton.instance) {
-      DynamoSingleton.instance = new DynamoDB.DocumentClient();
+  public static getInstance(): DynamoDBClient {
+    if (!DynamoDBClientSingleton.instance) {
+      DynamoDBClientSingleton.instance = new DynamoDBClient({
+        region: process.env.AWS_REGION || 'us-east-1',
+        credentials: {
+          accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
+          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
+        },
+      });
     }
-    return DynamoSingleton.instance;
+
+    return DynamoDBClientSingleton.instance;
   }
 }
 
-export default DynamoSingleton;
+export default DynamoDBClientSingleton;
