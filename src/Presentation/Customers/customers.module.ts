b22628624de4eee235b713acdda3a61c8e2c6ed6
rafaelpaesss <94455282+rafaelpaesss@users.noreply.github.com>
2024-12-03 21:08:51 -0300

    Update customers.module.ts

--- src/Presentation/Customers/customers.module.ts
+++ src/Presentation/Customers/customers.module.ts
@@ -2,7 +2,7 @@
 
 import { Module } from '@nestjs/common';
 import { CustomersController } from './customers.controller';  // Controller
-import { CustomerService } from '../../Application/services/customer.service';  // Corrigido: Serviço de lógica de clientes
+import { CustomersService } from '../../Application/services/customer.service';  // Corrigido: Serviço de lógica de clientes
 import { CustomersRepository } from '../../Domain/Repositories/customersRepository';  // Repositório de dados
 import { DynamoDBService } from '../../Infrastructure/Apis/dynamodb.service';  // Serviço de integração com DynamoDB
 import { ApiService } from '../../Infrastructure/Apis/api.service';  // Serviço da API
@@ -12,7 +12,7 @@ import { ApiService } from '../../Infrastructure/Apis/api.service';  // Serviço
   controllers: [CustomersController],  // Controller para mapear as rotas
   providers: [
     ApiService,  // Serviço que coordena as operações da API
-    CustomerService,  // Corrigido: Serviço de lógica de clientes (verifique o nome do arquivo e da classe)
+    CustomersService,  // Corrigido: Serviço de lógica de clientes (verifique o nome do arquivo e da classe)
     CustomersRepository,  // Repositório de dados
     DynamoDBService,  // Serviço de integração com o DynamoDB
   ],
