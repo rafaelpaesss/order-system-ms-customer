25e88b3ae9150f23806126069df411d275812772
rafaelpaesss <94455282+rafaelpaesss@users.noreply.github.com>
2024-12-03 21:13:55 -0300

    Update customers.adapter.ts

--- src/Domain/Adapters/customers.adapter.ts
+++ src/Domain/Adapters/customers.adapter.ts
@@ -10,7 +10,7 @@ export class CustomersAdapter implements CustomersRepository {
   async getCustomerById(id: number): Promise<Customers | null> {
     try {
       const params = {
-        TableName: 'Customers', // Substitua pelo nome da sua tabela no DynamoDB
+        TableName: 'Customers',
         Key: {
           id: { N: id.toString() },
         },
@@ -18,31 +18,37 @@ export class CustomersAdapter implements CustomersRepository {
       const result = await this.dynamoDBService.get(params);
       return result.Item ? (result.Item as Customers) : null;
     } catch (error) {
-      throw new Error(error.message);
+      if (error instanceof Error) {
+        throw new Error(`Error getting customer by ID: ${error.message}`);
+      }
+      throw new Error('Unknown error occurred while getting customer by ID.');
     }
   }
 
   async saveCustomer(customers: Customers): Promise<Customers> {
     try {
       const params = {
-        TableName: 'Customers', // Substitua pelo nome da sua tabela no DynamoDB
+        TableName: 'Customers',
         Item: customers,
       };
       await this.dynamoDBService.put(params);
       return customers;
     } catch (error) {
-      throw new Error(error.message);
+      if (error instanceof Error) {
+        throw new Error(`Error saving customer: ${error.message}`);
+      }
+      throw new Error('Unknown error occurred while saving customer.');
     }
   }
 
   async updateCustomer(customer: Customers): Promise<Customers> {
     try {
       const params = {
-        TableName: 'Customers', // Substitua pelo nome da sua tabela no DynamoDB
+        TableName: 'Customers',
         Key: {
           id: { N: customer.id.toString() },
         },
-        UpdateExpression: 'SET #name = :name, #email = :email', // Exemplo de campos para atualizar
+        UpdateExpression: 'SET #name = :name, #email = :email',
         ExpressionAttributeNames: {
           '#name': 'name',
           '#email': 'email',
@@ -56,14 +62,17 @@ export class CustomersAdapter implements CustomersRepository {
       const result = await this.dynamoDBService.update(params);
       return result.Attributes as Customers;
     } catch (error) {
-      throw new Error(error.message);
+      if (error instanceof Error) {
+        throw new Error(`Error updating customer: ${error.message}`);
+      }
+      throw new Error('Unknown error occurred while updating customer.');
     }
   }
 
-  async deleteCustomerById(id: number): Promise<Customers> {
+  async deleteCustomerById(id: number): Promise<Customers | null> {
     try {
       const params = {
-        TableName: 'Customers', // Substitua pelo nome da sua tabela no DynamoDB
+        TableName: 'Customers',
         Key: {
           id: { N: id.toString() },
         },
@@ -72,7 +81,10 @@ export class CustomersAdapter implements CustomersRepository {
       const result = await this.dynamoDBService.delete(params);
       return result.Attributes ? (result.Attributes as Customers) : null;
     } catch (error) {
-      throw new Error(error.message);
+      if (error instanceof Error) {
+        throw new Error(`Error deleting customer by ID: ${error.message}`);
+      }
+      throw new Error('Unknown error occurred while deleting customer by ID.');
     }
   }
 }
